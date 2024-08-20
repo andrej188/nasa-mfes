a74b20c40282015bf6eec8c43f455ad1783ab4fb
andrej188 <andrej.sazdovski12@gmail.com>
2024-08-20 12:34:25 +0200

    Update App.tsx

--- apps/shell/src/App.tsx
+++ apps/shell/src/App.tsx
@@ -1,10 +1,8 @@
-import React, { useRef, useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import homeWrapper from "home/homeWrapper";
 import listWrapper from "list/listWrapper";
-import detailsWrapper from "details/detailsWrapper";
-
 import "./index.scss";
 
 const Home = () => {
@@ -26,46 +24,38 @@ const List = () => {
     listWrapper(divRef.current);
   }, []);
   return (
-    <div ref={divRef}></div>
+    <div ref={divRef} className="h-full overflow-y-auto">
+      <div ref={divRef}></div>
+    </div>
   );
 };
 
-const Details = () => {
-  const divRef = useRef(null);
 
-  useEffect(() => {
-    detailsWrapper(divRef.current);
-  }, []);
-  return (
-    <div ref={divRef}></div>
-  );
-};
 
 
 const App = () => {
   return (
-      <Router>
-        <div className="flex flex-col h-screen">
-          <nav className="bg-gray-100 border-b border-gray-300 p-4">
-            <ul className="flex list-none p-0 m-0 space-x-4 text-xl">
-              <li>
-                <Link to="/" className="text-blue-500 hover:text-blue-700">Home</Link>
-              </li>
-              <li>
-                <Link to="/list" className="text-blue-500 hover:text-blue-700">List</Link>
-              </li>
-            </ul>
-          </nav>
-  
-          <div className="flex-1 overflow-hidden p-4">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/list" element={<List />} />
-              {/* <Route path="/details" element={<Details />} /> */}
-            </Routes>
-          </div>
+    <Router>
+      <div className="flex flex-col h-screen">
+        <nav className="bg-gray-100 border-b border-gray-300 p-4">
+          <ul className="flex list-none p-0 m-0 space-x-4 text-xl">
+            <li>
+              <Link to="/" className="text-blue-500 hover:text-blue-700">Home</Link>
+            </li>
+            <li>
+              <Link to="/list" className="text-blue-500 hover:text-blue-700">List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="flex-1 overflow-hidden p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/list" element={<List />} />
+          </Routes>
         </div>
-      </Router>
+      </div>
+    </Router>
   );
 
 };
